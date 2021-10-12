070640b6d8dcfc6a3a9e435d0eccfd3106e7fd84
kaemil <67312346+kaemil@users.noreply.github.com>
2021-10-12 21:29:19 +0200

    Updates

---
+++ precache-manifest.1e0cca7f389ac8519316b29d629e5125.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "812eff3b1b671452b64a",
+    "url": "/pokedex/static/css/main.523dc5a6.chunk.css"
+  },
+  {
+    "revision": "812eff3b1b671452b64a",
+    "url": "/pokedex/static/js/main.8065ebf1.chunk.js"
+  },
+  {
+    "revision": "499fa032a833d26aac94",
+    "url": "/pokedex/static/js/runtime~main.fa119ff4.js"
+  },
+  {
+    "revision": "fb69d855bedddb55e646",
+    "url": "/pokedex/static/js/2.2e9890e8.chunk.js"
+  },
+  {
+    "revision": "8505ff9d3914e9b78ee96706aa504241",
+    "url": "/pokedex/index.html"
+  }
+];
